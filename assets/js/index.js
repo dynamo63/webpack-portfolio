5a185619d47489d1825d33c6a1266c540463856a
dynamo63 <smoulongui@gmail.com>
2020-03-20 15:20:37 +0100

    Animation of menu links

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,24 @@
 
 const hamburgerBtn = document.querySelector('.nav-bars')
 const sidebarElt = document.querySelector('.nav-sidebar')
+const linkElts = document.querySelectorAll('.nav-link')
 
+/**
+ * Active la sidebar et les animations de chaque lien
+ */
 function activeSidebar() {
+    // Toogle class 
     sidebarElt.classList.toggle('nav-active')
+
+    // Animate link
+    linkElts.forEach((link,index) =>{
+        
+        if (link.style.animation) {
+            link.style.animation = ''
+        } else {
+            link.style.animation = `fadeInLeft .5s forwards ease ${index/6}s`
+        }
+    })
 }
 
 hamburgerBtn.addEventListener('click',activeSidebar)
