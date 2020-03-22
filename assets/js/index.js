22da492f9fe228a3f8ee92ecf6ab49b627765ae5
dynamo63 <smoulongui@gmail.com>
2020-03-22 14:29:06 +0100

    About Section Added

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,37 @@ function activeSidebar() {
     })
 }
 
+const slideParentElt = document.querySelector('.slides')
+// const div = document.querySelector('#about-me > h1')
+const slideElt = document.querySelectorAll('.slide')
+let moving = false
+let init = 0
+let currentPosit = null
+
+slideParentElt.addEventListener('pointerdown', (e) =>{
+    init = e.pageX
+    moving = true
+})
+
+slideParentElt.addEventListener('pointermove',(e) =>{
+
+    if (moving) {
+        currentPosit = e.pageX
+    }
+})
+
+slideParentElt.addEventListener('pointerup', (e) =>{
+    const diff = currentPosit - init
+    if (diff < 0) {
+        slideElt.forEach(slideItem =>{
+            slideItem.style.transform = `translateX(-100vw)`
+        })
+    } else {
+        slideElt.forEach(slideItem =>{
+            slideItem.style.transform = `translateX(0)`
+        })
+    }
+    // console.table(data)
+    moving = false
+    console.log(diff)
+})
