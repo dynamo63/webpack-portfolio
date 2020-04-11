a0d31420bd19911dd20747a7edf57f1c93da892d
dynamo63 <smoulongui@gmail.com>
2020-04-12 00:21:14 +0100

    Add Course Section

--- src/utils/func.js
+++ src/utils/func.js
@@ -9,7 +9,7 @@
 function activeSidebar (wrapper, links) {
     document.querySelector(wrapper).classList.toggle('nav-active')
     document.querySelectorAll(links).forEach((link, i) => {
-        link.style.animation = link.style.animation === '' ? `fadeInLeft .5s ease-out forwards ${i / 5 + 0.4}s` : ''
+        link.style.animation = link.style.animation === '' ? `fadeInLeft .5s ease-out forwards ${i / 5 + 0.2}s` : ''
     })
 }
 
