e0f77e5aec2ae478df1aaf8f6c065e61126e31bd
dynamo63 <smoulongui@gmail.com>
2020-04-21 14:58:40 +0100

    hotfix navbar: add Home link

--- src/utils/func.js
+++ src/utils/func.js
@@ -11,7 +11,7 @@ import { primaryColor } from '$css/variable.scss'
 function activeSidebar (wrapper, links) {
     document.querySelector(wrapper).classList.toggle('nav-active')
     document.querySelectorAll(links).forEach((link, i) => {
-        link.style.animation = link.style.animation === '' ? `fadeInLeft .5s ease-out forwards ${i / 5 + 0.2}s` : ''
+        link.style.animation = link.style.animation === '' ? `fadeInLeft .5s ease-out forwards ${i / 6}s` : ''
     })
 }
 
